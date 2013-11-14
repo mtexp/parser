6b95631545670258c434ed927213c3672cf7154f
mtexp <innovate.excellence+mt@gmail.com>
2013-11-14 09:28:34 +0000

    cleaned up script-links a little

--- script-links.sh
+++ script-links.sh
@@ -2,11 +2,11 @@
 GAMEPATH="/home/$USER/.steam/steam/SteamApps/common/Europa Universalis IV" 
 MODPATH="/home/$USER/dev/MEIOUandTaxes" 
 IFS=$'\n'
-FILES=`diff -qr "$GAMEPATH" "$MODPATH" | grep "Only in $GAMEPATH" | cut -c 73- | sed 's/: /\//g' | sed -e 's/^[ \t]*//'`
+FILES=`diff -qr "$GAMEPATH" "$MODPATH" | grep "Only in $GAMEPATH" | sed -e "s%Only in $GAMEPATH%%g" -e 's/: /\//g' -e 's/^[ \t]*//'`
 for file in $FILES
 do
-    ln -sf "$GAMEPATH/$file" "$MODPATH/$file"
-    echo "$file"
+  ln -sf "$GAMEPATH/$file" "$MODPATH/$file"
+  echo "$file"
 done
 
 #to undo:
